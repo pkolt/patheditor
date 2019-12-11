e144ae102a5e74f5e8983b01fbceeb301645d19d
pkolt <pkoltyshev@gmail.com>
2019-12-11 22:36:51 +0500

    Updates

--- precache-manifest.42825e0dc1aa0b1139e24e36a4143ab3.js
+++ precache-manifest.42825e0dc1aa0b1139e24e36a4143ab3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6592fe841c5c55d4e0f49fe4cb975599",
+    "revision": "94652895847a388199aedebc42b4e563",
     "url": "/patheditor/index.html"
   },
   {
-    "revision": "f33d482a56742e0de6fb",
+    "revision": "312cc6928f8918c5b1b5",
     "url": "/patheditor/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "f33d482a56742e0de6fb",
-    "url": "/patheditor/static/js/2.34af8764.chunk.js"
+    "revision": "312cc6928f8918c5b1b5",
+    "url": "/patheditor/static/js/2.dc1e8cde.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/patheditor/static/js/2.34af8764.chunk.js.LICENSE"
+    "url": "/patheditor/static/js/2.dc1e8cde.chunk.js.LICENSE"
   },
   {
-    "revision": "1157edee364d8434f41c",
-    "url": "/patheditor/static/js/main.3bc5a7f4.chunk.js"
+    "revision": "487ae70ecf44a2bc9183",
+    "url": "/patheditor/static/js/main.e94fb492.chunk.js"
   },
   {
     "revision": "06faf17d87f9afe6156b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/patheditor/precache-manifest.82450e4a217cc9f624218decbe1b8868.js"
+  "/patheditor/precache-manifest.42825e0dc1aa0b1139e24e36a4143ab3.js"
 );
 
 self.addEventListener('message', (event) => {
