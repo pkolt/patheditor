93112ace50a875ffac4af13b78d6a46045170c44
pkolt <pkoltyshev@gmail.com>
2019-12-11 14:22:43 +0500

    Updates

---
+++ precache-manifest.82450e4a217cc9f624218decbe1b8868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6592fe841c5c55d4e0f49fe4cb975599",
+    "url": "/patheditor/index.html"
+  },
+  {
+    "revision": "f33d482a56742e0de6fb",
+    "url": "/patheditor/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "f33d482a56742e0de6fb",
+    "url": "/patheditor/static/js/2.34af8764.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/patheditor/static/js/2.34af8764.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1157edee364d8434f41c",
+    "url": "/patheditor/static/js/main.3bc5a7f4.chunk.js"
+  },
+  {
+    "revision": "06faf17d87f9afe6156b",
+    "url": "/patheditor/static/js/runtime-main.0265d57d.js"
+  }
+]);
